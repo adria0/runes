d762f4f1f894cef66291926f38a3f30bb1682bd6
Adrià Massanet <amb@mac15.local>
2016-09-01 18:07:42 +0200

    Show entry files

--- store/model/entry.go
+++ store/model/entry.go
@@ -2,7 +2,8 @@ package model
 
 // Entry is a blog entry
 type Entry struct {
-	Workspace string `json:"workspace"`
-	ID        string `json:"id"`
-	Markdown  string `json:"-"` // the entry contents
+	Workspace string
+	ID        string
+	Markdown  string
+	Files     []string
 }
